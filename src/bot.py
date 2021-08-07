f9d3d0442ea37d83fac9250050ad1ee6a935365f
Pyrrhic <nefalt2@gmail.com>
2021-08-07 17:08:11 -0500

    Make model generated output work

--- src/bot.py
+++ src/bot.py
@@ -22,17 +22,25 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
-    elif message.content.startswith('$ss'):
-        text = gpt2.generate(sess, 
-              run_name='secret_scholars',
-              top_k=20,
-              nsamples=1,
-              batch_size=1,
-              temperature=1.4,
-              length = 150,
-              prefix = ""
-                    )
-        await message.channel.send(text)
+    elif message.content.startswith('$sss'):
+        if message.channel.name == 'bot-output' or message.channel.name == 'bot-testing':
+            prefix = message.content[5:]
+            text = gpt2.generate(sess, 
+                run_name='secret_scholars',
+                top_k=20,
+                nsamples=1,
+                batch_size=1,
+                temperature=1.4,
+                length = 150,
+                prefix = prefix,
+                return_as_list=True
+                        )[0]
+            # Replace code marks with a version with invisible spaces
+            text = text.replace('```', '`​`​`')
+            await message.channel.send(f'```{text}```')
+        else:
+            await message.channel.send('Please use the right channel :slight_smile:')
+        
 
 load_dotenv()
 print('starting bot')
