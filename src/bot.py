c5009eab51060c2aa79fb560607f06e491eaf3a3
Pyrrhic <nefalt2@gmail.com>
2021-08-07 22:12:31 -0500

    Add more actually working logging

--- src/bot.py
+++ src/bot.py
@@ -3,20 +3,29 @@ import os
 from dotenv import load_dotenv
 import gpt_2_simple as gpt2
 import logging
+from datetime import datetime, timezone
+
+# setup logging
 
 logging.basicConfig(level=logging.INFO)
 
+# setup gpt2
 sess = gpt2.start_tf_sess()
 gpt2.load_gpt2(sess, run_name='secret_scholars')
 
+total_generated = 0
+
+# discord bot
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    logging.info(f'{datetime.now(timezone.utc)} We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
+    global total_generated
+
     if message.author == client.user:
         return
 
@@ -25,6 +34,8 @@ async def on_message(message):
     elif message.content.startswith('$sss'):
         if message.channel.name == 'bot-output' or message.channel.name == 'bot-testing':
             prefix = message.content[5:]
+            logging.info(f'{datetime.now(timezone.utc)} Generating message with prefix "{prefix}". Previously generated count: {total_generated}')
+            total_generated += 1
             text = gpt2.generate(sess, 
                 run_name='secret_scholars',
                 top_k=20,
@@ -43,5 +54,4 @@ async def on_message(message):
         
 
 load_dotenv()
-print('starting bot')
 client.run(os.getenv('DISCORD_TOKEN'))
