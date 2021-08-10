ee6e2be6f74f93e04bd46dfc0b768387b6fa7fd7
Pyrrhic <nefalt2@gmail.com>
2021-08-10 17:37:10 -0500

    Convert indentation to 2 spaces

--- src/bot.py
+++ src/bot.py
@@ -16,41 +16,41 @@ conf = config.read_or_create_config()
 client = discord.Client()
 
 def allowed_channel(channel):
-    return channel in conf['allowed-channels']
+  return channel in conf['allowed-channels']
 
 @client.event
 async def on_ready():
-    logging.info(f'{datetime.now(timezone.utc)} We have logged in as {client.user}')
+  logging.info(f'{datetime.now(timezone.utc)} We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
-    global total_generated
+  global total_generated
 
-    if message.author == client.user:
-        return
+  if message.author == client.user:
+    return
 
-    if message.content.startswith('$hello'):
-        bot_response = await message.channel.send('Hello!')
-        await asyncio.sleep(300)
-        logging.info(f'{datetime.now(timezone.utc)} Attempting to delete hello messages')
-        await message.delete()
-        await bot_response.delete()
-    else:
-        for cmd in conf['checkpoints'].keys():
-            if message.content.startswith(f'${cmd}'):
-                if allowed_channel(message.channel.name):
-                    run_name = conf['checkpoints'][cmd]
-                    prefix = message.content[len(cmd) + 2:]
-                    logging.info(f'{datetime.now(timezone.utc)} Generating message with prefix "{prefix}" for model "{run_name}". Previously generated count: {total_generated}')
-                    total_generated += 1
-                    await gptlib.generate(run_name, prefix, message.channel.send)
-                else:
-                    bot_response = await message.channel.send('Please use the right channel :slight_smile:')
-                    await asyncio.sleep(300)
-                    logging.info(f'{datetime.now(timezone.utc)} Attempting to delete messages in incorrect channel')
-                    await message.delete()
-                    await bot_response.delete()
-        
+  if message.content.startswith('$hello'):
+    bot_response = await message.channel.send('Hello!')
+    await asyncio.sleep(300)
+    logging.info(f'{datetime.now(timezone.utc)} Attempting to delete hello messages')
+    await message.delete()
+    await bot_response.delete()
+  else:
+    for cmd in conf['checkpoints'].keys():
+      if message.content.startswith(f'${cmd}'):
+        if allowed_channel(message.channel.name):
+          run_name = conf['checkpoints'][cmd]
+          prefix = message.content[len(cmd) + 2:]
+          logging.info(f'{datetime.now(timezone.utc)} Generating message with prefix "{prefix}" for model "{run_name}". Previously generated count: {total_generated}')
+          total_generated += 1
+          await gptlib.generate(run_name, prefix, message.channel.send)
+        else:
+          bot_response = await message.channel.send('Please use the right channel :slight_smile:')
+          await asyncio.sleep(300)
+          logging.info(f'{datetime.now(timezone.utc)} Attempting to delete messages in incorrect channel')
+          await message.delete()
+          await bot_response.delete()
+    
 
 load_dotenv()
 client.run(os.getenv('DISCORD_TOKEN'))
--- src/gptlib.py
+++ src/gptlib.py
@@ -10,47 +10,47 @@ current_run_name = None
 lock = asyncio.Lock()
 
 def start_sess(run_name):
-    global sess
-    global session_generated
-    global current_run_name
+  global sess
+  global session_generated
+  global current_run_name
 
-    if not sess is None:
-        logging.info(f'{datetime.now(timezone.utc)} Restarting tf session')
-        tf.compat.v1.reset_default_graph()
-        sess.close()
+  if not sess is None:
+    logging.info(f'{datetime.now(timezone.utc)} Restarting tf session')
+    tf.compat.v1.reset_default_graph()
+    sess.close()
 
-    sess = gpt2.start_tf_sess()
-    gpt2.load_gpt2(sess, run_name=run_name)
-    session_generated = 0
-    current_run_name = run_name
+  sess = gpt2.start_tf_sess()
+  gpt2.load_gpt2(sess, run_name=run_name)
+  session_generated = 0
+  current_run_name = run_name
 
 async def generate(run_name, prefix, callback):
-    global session_generated
-    global current_run_name
+  global session_generated
+  global current_run_name
 
-    async with lock:
-        if not run_name == current_run_name:
-            logging.info(f'{datetime.now(timezone.utc)} Changing from model {current_run_name} to {run_name}')
-            start_sess(run_name)
+  async with lock:
+    if not run_name == current_run_name:
+      logging.info(f'{datetime.now(timezone.utc)} Changing from model {current_run_name} to {run_name}')
+      start_sess(run_name)
 
-        text = gpt2.generate(sess, 
-            run_name=current_run_name,
-            top_k=20,
-            nsamples=1,
-            batch_size=1,
-            temperature=1.4,
-            length = 150,
-            prefix = prefix,
-            return_as_list=True
-                    )[0]
-        # Replace code marks with a version with invisible spaces
-        text = text.replace('```', '`​`​`')
-        await callback(f'```{text}```')
+    text = gpt2.generate(sess, 
+      run_name=current_run_name,
+      top_k=20,
+      nsamples=1,
+      batch_size=1,
+      temperature=1.4,
+      length = 150,
+      prefix = prefix,
+      return_as_list=True
+          )[0]
+    # Replace code marks with a version with invisible spaces
+    text = text.replace('```', '`​`​`')
+    await callback(f'```{text}```')
 
-        session_generated += 1
+    session_generated += 1
 
-        if session_generated >= 2:
-            logging.info(f'{datetime.now(timezone.utc)} 2 sessions generated, restarting tf session')
-            start_sess(current_run_name)
+    if session_generated >= 2:
+      logging.info(f'{datetime.now(timezone.utc)} 2 sessions generated, restarting tf session')
+      start_sess(current_run_name)
 
-        gc.collect()
+    gc.collect()
