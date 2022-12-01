2928754440aa01b70cced76905fa0d1783c59298
nfaltermeier <nefalt2@gmail.com>
2022-11-30 20:05:43 -0600

    Replace twitter links with vxtwitter

---
+++ src/twitter.py
@@ -0,0 +1,16 @@
+import discord
+
+async def on_message(message: discord.Message, conf):
+  if conf.enforce_vxtwitter:
+    base_len = 19
+    i = message.content.find('https://twitter.com')
+    if i == -1:
+      base_len = 18
+      i = message.content.find('http://twitter.com')
+    if i != -1:
+      space = message.content.find(" ", i)
+      if space == -1:
+        space = len(message.content)
+      url = f'https://vxtwitter.com{message.content[i + base_len: space]}'
+      await message.edit(suppress=True)
+      await message.reply(url)
